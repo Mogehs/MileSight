3cedd86074c789a45bc143a19250e7c124806f3b
Mogehs <mugheesurrehman06@gmail.com>
2025-02-25 17:00:48 +0500

    error resolved

--- src/components/solutions/energy-effecieny/EnergyEffeciency.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EnergyEffeciency = () => {
-  return <div>EnergyEffeciency</div>;
-};
-
-export default EnergyEffeciency;
--- src/components/solutions/smart-building/SmartBuilding.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SmartBuilding = () => {
-  return <div>SmartBuilding</div>;
-};
-
-export default SmartBuilding;
