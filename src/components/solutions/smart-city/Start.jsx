ca8c6b9b95bfb7aab0e019175b3cba413565ea82
Fakhar Abbas Bhatti <fakharabbasbhatti333@gmail.com>
2025-02-26 17:33:49 +0500

    smart city

--- src/components/solutions/smart-city/Start.jsx
+++ src/components/solutions/smart-city/Start.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Start = () => {
-  return <div>Start</div>;
+  return (
+    <div
+      id="hero-section"
+      className="relative h-screen flex items-center justify-start bg-cover bg-center"
+      style={{ backgroundImage: "url('/SmartCity/star.jpg')" }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0  bg-opacity-50"></div>
+
+      {/* Hero Content */}
+      <div className="relative text-center text-white px-8 flex flex-col justify-center md:gap-4 gap-2 md:items-start items-center overflow-hidden">
+        <h1 className="text-[2rem] md:text-[3rem] font-medium">
+        Smart City Solution
+        </h1>
+        <p className="text-xl md:text-2xl font-base">
+        Realize a More Connected, Livable and Efficient City
+         </p>
+        </div>
+    </div>
+  );
 };
 
 export default Start;
---
+++ src/components/solutions/smart-city/SuccessStories.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const buttonItems = [
+  { label: "School", image: "./SmartCity/bt-2.jpg" },
+  { label: "Building", image: "./SmartCity/bt-2.jpg" },
+  { label: "Hospital", image: "./SmartCity/bt-3.jpg" },
+  { label: "Utility", image: "./SmartCity/bt-4.jpg" },
+  { label: "Museum", image: "./SmartCity/bt-5.jpg" },
+  { label: "Industrial", image: "./SmartCity/bt-5.jpg" },
+];
+
+const SuccessStories = () => {
+  const [selectedImage, setSelectedImage] = useState(buttonItems[0].image);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div className="my-20 hidden md:block" id="smartCity-benefit">
+        <h1 className="text-center text-2xl font-bold mb-5">Success Story</h1>
+      <div className="flex justify-center gap-4">
+        {buttonItems.map((button, index) => (
+          <button
+            key={index}
+            className={`px-4 py-2 rounded-xl text-[10px] transition-all border border-transparent hover:bg-[#0299f4] ${
+              activeIndex === index
+                ? "bg-[#0299f4] text-black"
+                : "border-black text-black"
+            }`}
+            onClick={() => {
+              setSelectedImage(button.image);
+              setActiveIndex(index);
+            }}
+          >
+            {button.label}
+          </button>
+        ))}
+      </div>
+      {selectedImage && (
+        <div className="mt-5 flex justify-center">
+          <img src={selectedImage} alt="Selected" className="w-[40rem] h-64 rounded-md object-cover" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SuccessStories;
