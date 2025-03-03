76777d3d81a3a29b4dcdf33b38556c439baa4fbe
Mogehs <mugheesurrehman06@gmail.com>
2025-03-03 12:04:55 +0500

    NDAA compilant page update

--- src/components/products/iot-sensing/NDAA.jsx
+++ src/components/products/iot-sensing/NDAA.jsx
@@ -67,10 +67,26 @@ const NDAA = () => {
               applications where compliance is essential
             </p>
             <div className="my-2 md:my-5 lg:mt-10 flex gap-2 sm:gap-5 flex-col sm:flex-row">
-              <button className="bg-[#7CCA9A] w-fit h-fit text-nowrap text-[0.6rem] md:text-lg p-[5px] rounded-md">
+              <button
+                className="bg-[#7CCA9A] w-fit h-fit text-nowrap text-[0.6rem] md:text-sm p-[5px] rounded-md cursor-pointer"
+                onClick={() =>
+                  window.open(
+                    "/instructions-pdf/milesight-network-camera-quick-start-guide.pdf",
+                    "_blank"
+                  )
+                }
+              >
                 Download Brochure
               </button>
-              <button className="bg-[#7CCA9A] text-[0.6rem] sm:text-lg p-[5px] rounded-md  w-fit h-fit text-nowrap">
+              <button
+                className="bg-[#7CCA9A] text-[0.6rem] sm:text-sm p-[5px] rounded-md  w-fit h-fit text-nowrap cursor-pointer"
+                onClick={() =>
+                  window.open(
+                    "/instructions-pdf/milesight-network-camera-quick-start-guide.pdf",
+                    "_blank"
+                  )
+                }
+              >
                 Download NDAA Compilant Product List
               </button>
             </div>
@@ -86,7 +102,7 @@ const NDAA = () => {
             <div className=" w-full mx-auto sm:w-11/12 h-fit sm:mt-6 mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
               {data.map((item, index) => (
                 <div
-                  className=" w-full h-fit flex px-2 flex-col lg:flex-row justify-center items-center pr-2  border border-gray-300 rounded-md cursor-pointer hover:shadow hover:shadow-gray-400 gap-20"
+                  className=" w-full h-fit flex px-2 flex-col lg:flex-row justify-center items-center pr-2  border border-gray-300 rounded-md hover:shadow hover:shadow-gray-400 gap-20"
                   key={index}
                 >
                   <img src={item.img} alt="missing" className="h-40" />
@@ -96,11 +112,23 @@ const NDAA = () => {
                       {item.title}
                     </h1>
                     <ul className=" mt-2 text-sm text-gray-500 list-disc marker:text-[#00667C] ">
-                      <li className="text-xs">{item.li}</li>
-                      <li className="text-xs">{item.li1}</li>
-                      <li className="text-xs">{item.li3}</li>
-                      <li className="text-xs">{item.li4}</li>
-                      <li className="text-xs">{item.li5}</li>
+                      <a href="/products/ai-motorized-mini-dome">
+                        <li className="text-xs hover:text-[#7CCA9A]">
+                          {item.li}
+                        </li>
+                      </a>
+                      <li className="text-xs hover:text-[#7CCA9A]">
+                        {item.li1}
+                      </li>
+                      <li className="text-xs hover:text-[#7CCA9A]">
+                        {item.li3}
+                      </li>
+                      <li className="text-xs hover:text-[#7CCA9A]">
+                        {item.li4}
+                      </li>
+                      <li className="text-xs hover:text-[#7CCA9A]">
+                        {item.li5}
+                      </li>
                     </ul>
                     <div className=" flex justify-end pr-4  items-center gap-2 mt-4">
                       <span className=" text-xs sm:text-xl">{item.icon}</span>
@@ -110,8 +138,16 @@ const NDAA = () => {
               ))}
             </div>
             <div className="flex justify-center my-10 text-white ">
-              <button className="bg-[#00667C] text-[0.6rem] sm:text-sm p-[5px] rounded-md  w-fit h-fit text-nowrap">
-                Download NDAA Compilant Product List
+              <button
+                className="bg-[#00667C] text-[0.6rem] sm:text-sm p-[5px] rounded-md w-fit h-fit text-nowrap cursor-pointer"
+                onClick={() =>
+                  window.open(
+                    "/instructions-pdf/milesight-network-camera-quick-start-guide.pdf",
+                    "_blank"
+                  )
+                }
+              >
+                Download NDAA Compliant Product List
               </button>
             </div>
           </div>
