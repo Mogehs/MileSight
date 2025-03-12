78f99cd001af7aaa56ff3e4dc89f589ee16dc098
Mogehs <mugheesurrehman06@gmail.com>
2025-03-12 16:09:01 +0500

    sameSite

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -39,6 +39,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
     });
 
     res.json({ message: "Logged in successfully" });
