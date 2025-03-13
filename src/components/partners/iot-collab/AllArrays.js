91d157ba66cc364bf3f67b58603a1270d2bf2bcd
asad3473 <asadsoomro0311@gmail.com>
2025-03-13 20:09:04 +0500

    iot  collaboration

---
+++ src/components/partners/iot-collab/AllArrays.js
@@ -0,0 +1,117 @@
+export const arrayData = [
+    { title: "Make Sensing Matter" },
+    {
+        title: "Value-Driven Reasons to Join Forces",
+        subtitle: "Strong R&D Capability",
+        description:
+            "Thorough Teamwork Far-Sighted Marketing Strategy Thriving Business Ecosystem Impacts on the Ecosystem",
+    },
+    {
+        title: "Mutually Beneficial Collaboration",
+        subtitle: "Win-win Channel Collaboration",
+        description: "Application Empowerment",
+    },
+    {
+        title: "Make It More Visionary",
+        subtitle: "Product Application Matrix",
+        description:
+            "People Sensing Driven Smart Building Start the Collaboration",
+    },
+    { title: "Milesight Partner Ecosystem", img1: "/IotCollaboration/one.jpg" },
+    { title: "Milesight Channel Partners", img1: "/IotCollaboration/two.jpg" },
+    { title: "Milesight Partner Program", img1: "/IotCollaboration/three.jpg" },
+    { title: "Technology Partner Program", img1: "/IotCollaboration/four.jpg" },
+];
+
+
+export const secondDiv = [
+    {
+        title: "Strong R&D Capability-Proven Expertise and Reliability", description: "Milesight is an R&D-driven technology company with a strong focus on research and development. We excel in swiftly responding to market and customer needs, efficiently transforming these requirements into effective product solutions. Precision and speed are our hallmarks.",
+        secondimg: "/IotCollaboration/blue.jpg", Chils: [
+            {
+                nbr: "30+", descrip: "Categories in 4 years",
+            },
+            {
+                nbr: "15%", descrip: "Sales Revenue Invested Into R&D",
+            },
+            {
+                nbr: "56%", descrip: "R&D colleagues",
+            },
+            {
+                nbr: "4.5 Months", descrip: "Average new product launching cycle",
+            },
+        ]
+    }
+];
+
+
+export const thirdDiv = [
+    {
+        img: '/IotCollaboration/presale.png', descrip: [
+            {
+                descripData: "Sales Team: professional support in developing and identifying opportunities"
+            },
+            {
+                descripData: "Pre-sales Team: bring advisory and consultancy services"
+            },
+            {
+                descripData: "Marketing Team: bring comprehensive promotional service and enhance visibility and brand awareness"
+            },
+            {
+                descripData: "Technical support Team: bring seamless after-sales service"
+            },
+        ]
+    }
+];
+
+export const lastDiv = [
+    {
+        title: "iBox IAQ Kit", subtitle: "Indoor Air Quality Solution", img: "/IotCollaboration/img1.png", Data: [
+            "AM316",
+            "AM307", "UG56",
+            "AM103",
+            "AM102", "Milesight IoT Cloud"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+    {
+        title: "iBox Smart Building Kit", subtitle: "Smart Building Solution", img: "/IotCollaboration/img1.png", Data: [
+            "WS101",
+            "WS303 ", "UG56",
+            "WS301",
+            "WS558", "Milesight IoT Cloud","AM103","UC300"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+    {
+        title: "iBox CoWork Kit", subtitle: "Smart Office Solution", img: "/IotCollaboration/img2.png", Data: [
+            "VS121",
+            "VS340 ", 
+            "WS558", "Milesight IoT Cloud","DS3604 ","UG56"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+    {
+        title: "iBox CoWork Kit", subtitle: "Smart Office Solution", img: "/IotCollaboration/img2.png", Data: [
+            "VS121",
+            "WS101 ", 
+            "UG56","WS301","UC100","WS156", "Milesight IoT Cloud"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+    {
+        title: "iBox Smart Restroom Kit", subtitle: "Smart Restroom Solution", img: "/IotCollaboration/img3.png", Data: [
+            "WS101",
+            "WS303  ", 
+            "UG56","WS301","WS558","WS156", "Milesight IoT Cloud","AM103","UC300"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+    {
+        title: "iBox Smart Agriculture Kit", subtitle: "Smart Agriculture Solution", img: "/IotCollaboration/img4.png", Data: [
+            "EM500-SMTC",
+            "UG56", "Milesight IoT Cloud","UC511"
+        ],
+        descrip:"*Enjoy free cloud services for a specified period of time"
+    },
+]
