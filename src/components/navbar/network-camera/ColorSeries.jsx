4455f40ab573755a2ae2b2af603aefb10022824a
Mogehs <mugheesurrehman06@gmail.com>
2025-03-14 20:58:02 +0500

    PICTURES ISSUE

--- src/components/navbar/network-camera/ColorSeries.jsx
+++ src/components/navbar/network-camera/ColorSeries.jsx
@@ -1,15 +1,17 @@
-import img1 from "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-70.png";
-import img2 from "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-71.png";
-import img3 from "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-72.png";
-
 export default function ColorSeries() {
   const cameraArray = [
     {
       name: "AI Color+ Vandal-proof Mini Bullet Network Camera-70",
-      img: img1,
+      img: "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-70.png",
     },
-    { name: "AI Color+ Vandal-proof Mini Bullet Network Camera-71", img: img2 },
-    { name: "AI Color+ Vandal-proof Mini Bullet Network Camera-72", img: img3 },
+    {
+      name: "AI Color+ Vandal-proof Mini Bullet Network Camera-71",
+      img: "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-71.png",
+    },
+    {
+      name: "AI Color+ Vandal-proof Mini Bullet Network Camera-72",
+      img: "/client-pic/Color Series/AI Color+ Vandal-proof Mini Bullet Network Camera-72.png",
+    },
   ];
 
   return (
